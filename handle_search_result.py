d81e4d9fbf6629cbe9515d3ef33ca7d833d7437f
jerryboy1031 <jerryboy10311@gmail.com>
2023-07-04 16:30:54 +0800

    new file:   handle_search_result.py 	modified:   login_fb.py

---
+++ handle_search_result.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 16:14:22 2023
+
+@author: User
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import csv
+
+def saveTOcsv(data):
+    # Save data to CSV file
+    csv_file = "google_search_results.csv"
+    with open(csv_file, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Title", "URL", "Snippet"])  # Header row
+        writer.writerows(data)
+        
+def extract_website():
+    # Configure Selenium
+    driver_path = "/chromedriver_win32/chromedriver.exe" # Path to your ChromeDriver executable
+    service = Service(driver_path)  
+    options = Options()
+    #options.headless = True  # Run the browser in headless mode (without GUI)
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Define search query
+    search_query = "法國自由行景點"  # Enter your desired search query
+
+    # Perform search and extract data
+    driver.get(f"https://www.google.com/search?q={search_query}")
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # Find search result elements
+    search_results = soup.find_all("div", class_="yuRUbf")
+
+    # Extract data from search results
+    results_data = []
+    for result in search_results[:10]:
+        title = result.find("h3").get_text()
+        url = result.find("a")["href"]
+        snippet = result.find("span", class_="aCOpRe").get_text()
+        results_data.append([title, url, snippet])
+        
+    #saveTOcsv(data_results)
+    
+    # Close the browser
+    driver.quit()
+
+    #print("Scraping completed and data saved to", csv_file)
+    print(results_data)
+
+
+if __name__ == '__main__':
+    extract_website()
--- login_fb.py
+++ login_fb.py
@@ -4,22 +4,26 @@ from selenium.webdriver.chrome.options import Options
 import time
 from urllib.request import urlopen
 
-driver_path = "/chromedriver_win32/chromedriver.exe" # the path of chromedriver.exe
-service= Service(driver_path)
-option = Options()
-#操作網頁是不顯示網頁 manipulate the browser invisibly
-#option.add_argument("-headless")
-chrome = webdriver.Chrome(service=service, options=option)
-chrome.get("https://www.facebook.com/")
+def login_fb():
+    driver_path = "/chromedriver_win32/chromedriver.exe" # the path of chromedriver.exe
+    service= Service(driver_path)
+    option = Options()
+    #操作網頁是不顯示網頁 manipulate the browser invisibly
+    #option.add_argument("-headless")
+    chrome = webdriver.Chrome(service=service, options=option)
+    chrome.get("https://www.facebook.com/")
 
-email = chrome.find_element("id", "email")
-password = chrome.find_element("id", "pass")
+    email = chrome.find_element("id", "email")
+    password = chrome.find_element("id", "pass")
 
-time.sleep(1)
-email.send_keys('jerryboy1031@yahoo.com.tw')
-password.send_keys('zxcvbnm1031')
-time.sleep(3)
-password.submit()
-time.sleep(5)
-#關閉瀏覽器 close the browser
-chrome.close()
+    time.sleep(1)
+    email.send_keys('jerryboy1031@yahoo.com.tw')
+    password.send_keys('zxcvbnm1031')
+    time.sleep(3)
+    password.submit()
+    time.sleep(5)
+    #關閉瀏覽器 close the browser
+    chrome.close()
+
+if __name__ == '__main__':
+    login_fb()
